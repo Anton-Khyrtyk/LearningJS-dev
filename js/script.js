9c978dc2dc91501c1e46bc8890a3317c86961d1d
Anton-Khyrtyk <anton.khytryk@gmail.com>
2024-11-29 17:59:16 +0200

    Lecture 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// let a = 20;
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+// const b = 25;
+// console.log(b);
+
+// const value=0
+// console.log(Boolean(value))
+// console.log(typeof Boolean(value))
+
+// const num=98463846843134484;
+// console.log(typeof num);
+
+console.log(Math.random()*(4-2)+2);
