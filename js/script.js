e7724eabe5fcb919e7eca10438524fca1083a766
Anton-Khyrtyk <anton.khytryk@gmail.com>
2024-12-02 14:52:00 +0200

    next part

--- js/script.js
+++ js/script.js
@@ -13,4 +13,69 @@
 // const num=98463846843134484;
 // console.log(typeof num);
 
-console.log(Math.random()*(4-2)+2);
+//console.log(Math.random()*(4-2)+2);
+
+//Lecture 2
+
+const message = "JavaScript is awesome!"
+
+//console.log(message);
+
+//alert(message);
+
+// const isConfirm = confirm("Please confirm reservation");
+// console.log(isConfirm);
+
+// const test = confirm("Yes or no")
+// console.log(test);
+
+//prompt
+
+// const userName = prompt("Please enter your name");
+// console.log(userName);
+
+let value;
+
+// ---String---//
+
+// value = false;
+// console.log(typeof value);
+// const newValue = String(value);
+// console.log(typeof newValue);
+
+
+// ---Number--- //
+
+// value = "123";
+// console.log(typeof value);
+
+// const newValue = Number(value);
+// console.log(typeof newValue);
+
+// const test = Number("qwerty")
+// console.log(test)
+
+// Остаток от деления //
+
+//console.log(8%3); // 3
+//console.log(10%3); // 1
+//console.log(10%2); // 0
+
+// Возведение в степень
+
+// console.log(5 ** 2); //25
+
+// let a = 2;
+// let b = 3;
+// let c;
+// c=a+b;
+// console.log(c);
+
+// const x = Number(prompt("Please enter number"));
+// const y = Number(prompt("Please enter one more number"));
+// let d = x+y;
+// console.log("the sum is "+d);
+// alert(d);
+
+let val = "0";
+console.log(val == 0);
