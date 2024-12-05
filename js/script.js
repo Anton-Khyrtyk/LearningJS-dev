d6196fad013fecc6814b65fb22fadc43fbd8e52b
Anton-Khyrtyk <anton.khytryk@gmail.com>
2024-12-05 15:34:02 +0200

    Home task added

--- js/script.js
+++ js/script.js
@@ -207,3 +207,72 @@
 // console.log(summ);
 
 // ENd of lecture 3
+
+// Home task 
+
+//  -- 1 --
+// У вас є масив об’єктів fruts, і в кожному з них є name
+// Напишіть код, який перетворює їх в масив імен.
+
+// const fruts = [
+//   { id: 0, name: "Apple" },
+//   { id: 1, name: "Tomat" },
+//   { id: 2, name: "Cherry" },
+//   { id: 3, name: "Orange" },
+// ];
+
+// let result = fruts.map((item) => item.name);
+// console.log(result);
+
+//  -- 2 --
+//Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (let a = 2; a <= 10; a++ ){
+//     if (a % 2 != 0) continue;
+//     console.log(a);
+// }
+
+
+//  -- 3 --
+//Замініть цикл "for" на "while"
+
+// for (let i = 0; i < 5; i++) {
+//     console.log( `цифра ${i}!` );
+//   }
+
+// let a = 0;
+// while (a < 5){
+//     console.log( `цифра ${a}!` );
+//     a++;
+// }
+
+
+
+//  -- 4 --
+//Напишіть цикл, який пропонує prompt ввести число більше за 100.
+//Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
+//Цикл повинен запитувати число доти, доки відвідувач не введе число,
+// більше за 100, або не скасує ввід/введе порожній рядок.
+
+
+// while (true){
+//     let value = +prompt("Enter the number more than 100");
+//     if (value > 100 || !value) break;
+// }
+
+
+//  -- 5 --
+// Вирахуйте середній вік
+
+// const girls = [
+//   { age: 23, name: "Оля" },
+//   { age: 29, name: "Аня" },
+//   { age: 10, name: "Юля" },
+//   { age: 20, name: "Катя" },
+// ];
+
+// let ages = girls.map((item) => item.age)
+// console.log(ages)
+
+// let summ = ages.reduce((sum, i) => sum + i) / ages.length;
+// console.log(summ);
