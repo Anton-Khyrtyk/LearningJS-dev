9efcf40a97d881368b468eeb6926e4e94305da96
Anton-Khyrtyk <anton.khytryk@gmail.com>
2024-12-05 14:38:14 +0200

    Second part of Lecture 3

--- js/script.js
+++ js/script.js
@@ -115,3 +115,95 @@
 // console.log(matrix[0][1]);
 
 // End of first part Time 28:55
+
+
+// --- Методи присвоєння ---
+
+// let a = 5;
+// let b = a;
+
+// a = 10;
+
+// console.log(a);
+// console.log(b);
+
+// let arr = [1, 2, 3];
+// let arr2 = arr;
+
+// arr.push(4);
+
+// console.log(arr2);
+
+
+// --- методи масиву
+
+// let arr = ["a", "b", "c", "d", "f"];
+
+// arr.splice(2,2);
+
+// console.log(arr);
+
+// console.log(arr.slice(1,3));
+
+// let a = [1, 2, 3];
+
+// console.log(arr.concat(a));
+
+
+// console.log(arr.indexOf("c"));
+
+// console.log(arr.indexOf("c", 3));
+
+//console.log(arr.lastIndexOf("c"));
+
+// console.log(arr.includes("c"));
+
+
+// --- find
+
+// const fruits = [
+//     {id: 0, name: "Apple"},
+//     {id: 1, name: "Tomato"},
+//     {id: 2, name: "Cherry"},
+//     {id: 3, name: "Orange"}
+// ];
+
+// console.log(fruits.find((item) => item.id === 2));
+
+// --- filter
+
+// console.log(fruits.filter((item) => item.id < 2));
+
+
+// --- map
+
+// let result = fruits.map(item => item.name.length);
+// console.log(result);
+
+// --- split
+
+// let names = "Оля, Юля, Аня, Петя";
+
+// let arr = names.split(", ");
+
+// // console.log(arr);
+
+// --- join
+
+// let newString = arr.join("; ");
+
+// console.log(newString);
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let red = arr.reduce((summ, item) => summ + item);
+
+// console.log(red);
+
+// let arr = [23, 45, 30];
+
+// let summ = arr.reduce((i, item) => i + item) / arr.length;
+// console.log(summ);
+
+// ENd of lecture 3
